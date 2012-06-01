737b1d5c4cc0cc2cdf0557688598393d78140831
aligh <aghasizadeh70@gmail.com>
2012-06-01 12:39:16 +0430

    fix the slideshow bug :D

--- aligh/script.js
+++ aligh/script.js
@@ -1,38 +1,20 @@
 $(function(){
-	function slideshow(){
-	$('.img4').fadeOut(2800,null,function(){
-			$('.img3').fadeOut(2800,null,function(){
-				$('.img2').fadeOut(2800,null,function(){
-					$('.img1').fadeIn(2800,null,function(){
-						$('.img2').fadeIn(2800,null,function(){
-							$('.img3').fadeIn(2800,null,function(){
-								$('.img4').fadeIn(2800,null,function(){
-									$('.img4').fadeOut(2800)
-								})
-							})
-						})
-					})
-				})	
-			})
-		})
-	
-	/*	$('.img4').animate({opacity:0},1000,null,function(){
-		$('.img3').animate({opacity:0},1000,null,function(){
-			$('.img2').animate({opacity:0},1000,null,function(){
-				$('.img1').animate({opacity:0},1000,null,function(){
-					$('.img2').animate({opacity:1},1000,null,function(){
-						$('.img3').animate({opacity:1},1000,null,function(){
-							$('.img4').animate({opacity:1},1000,null,function(){
-								$('.img1').animate({opacity:1},1000)
-							})
-						})
-					})
-				})
-			})
-		})
-	})*/
-	}
-	//slideshow()
-	setInterval(slideshow,1000);
-	
+  function slideshow(){
+    $('.img4').fadeOut(2800,null,function(){
+      $('.img3').fadeOut(2800,null,function(){
+        $('.img2').fadeOut(2800,null,function(){
+          $('.img1').fadeIn(2800,null,function(){
+            $('.img2').fadeIn(2800,null,function(){
+              $('.img3').fadeIn(2800,null,function(){
+                $('.img4').fadeIn(2800,null,function(){
+                  slideshow();
+                })
+              })
+            })
+          })
+        })  
+      })
+    })
+  }
+  slideshow();
 });
