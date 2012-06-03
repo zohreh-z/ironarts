70eb59932d9695069739ce74038a170eac02e191
aligh <aghasizadeh70@gmail.com>
2012-06-03 22:19:58 +0430

    got a new idea for sheets

--- aligh/script.js
+++ aligh/script.js
@@ -1,20 +1,28 @@
 $(function(){
-  function slideshow(){
-    $('.img4').fadeOut(4000,null,function(){
-      $('.img3').fadeOut(4000,null,function(){
-        $('.img2').fadeOut(4000,null,function(){
-          $('.img1').fadeIn(4000,null,function(){
-            $('.img2').fadeIn(4000,null,function(){
-              $('.img3').fadeIn(4000,null,function(){
-                $('.img4').fadeIn(4000,null,function(){
-                  slideshow();
-                })
-              })
-            })
-          })
-        })  
-      })
-    })
-  }
-  slideshow();
+	function slideshow(){
+		$('.img4').fadeOut(4000,null,function(){
+			$('.img3').fadeOut(4000,null,function(){
+				$('.img2').fadeOut(4000,null,function(){
+					$('.img1').fadeIn(4000,null,function(){
+						$('.img2').fadeIn(4000,null,function(){
+							$('.img3').fadeIn(4000,null,function(){
+								$('.img4').fadeIn(4000,null,function(){
+									slideshow();
+								})
+							})
+						})
+					})
+				})  
+			})
+		})
+	}
+	slideshow();
+  
+	$('.about-li').click(function(){
+		$('.bottom-background').css('display','block');
+		$('body').css('overflow','auto');
+		$('.about-content').animate({marginLeft:0},500);
+	})
+
+  
 });
