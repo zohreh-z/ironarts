3ca6687a59a6516f0c3949c4be32362cbbab0846
aligh <aghasizadeh70@gmail.com>
2012-06-16 12:02:12 +0430

    remove a junk part

--- aligh/about.php
+++ aligh/about.php
@@ -38,29 +38,7 @@
 					<h1>consectetuer</h1>
 					<p>Praesent vestibulum molestie lacus. Aenean nonummy 1999 hendrerit mauris. Phasellus porta. Fusce sucipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus 2001 mus. Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maecenas 2011 tristique.</p>
 				</div>
-				<div class="clear"></div>
-				
-				<div class="content-news">
-					<h1>NEWS</h1>
-					<p>Aenean nec eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse sollicitudin velit sed leo. Ut pharetra augue nec augue. Nam elit magna, hendrerit sit amet, tincidunt ac, viverra sed, nulla. <br />
-						<br />
-						<br />
-						<br />
-					</p>
-					<div class="news-1 left w5">
-						<h2>26 december 2011</h2>
-						<p>Ereet am leot tellu apibus eget elementum vel cursus eleife elitenean auctowisi et urnat volutpas ac tur. </p>
-					</div>
-					<div class="news-2 left w5">
-						<h2>23 december 2011</h2>
-						<p>Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur.</p>
-					</div>
-					<div class="news-3 ml10 last">
-						<h2>20 december 2011</h2>
-						<p>Aenean nec eros. Vestibulum ante ipsu primis in faucibus orci luctus et ultrices posuere cubilia Curae. </p>
-					</div>
-					<div class="clear"></div>
-				</div>
+				<div class="clear"></div>				
 			</div>
 	</div>
 
